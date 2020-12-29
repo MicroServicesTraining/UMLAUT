fea0b3b6324ab7dad1fb53c21b03cfa6acf8c862
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2020-12-29 08:55:18 +0530

    Spring Boot Security basic demo app

---
+++ mm-sb-sec-basic/src/main/java/com/mm/sb/sec/demo/basic/MmSbSecBasicApplication.java
@@ -0,0 +1,13 @@
+package com.mm.sb.sec.demo.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSbSecBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSbSecBasicApplication.class, args);
+	}
+
+}
---
+++ mm-sb-sec-basic/src/test/java/com/mm/sb/sec/demo/basic/MmSbSecBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.sb.sec.demo.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmSbSecBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
