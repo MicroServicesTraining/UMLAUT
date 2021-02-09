2f45c680e84496331835036229603e8bfa80c44a
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2021-02-09 05:48:44 +0530

    jwt demo

---
+++ mm-sb-jwt-demo/src/main/java/com/mm/jwt/demo/MMSBJwtDemoApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.mm.jwt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dorak
+ *
+ */
+@SpringBootApplication
+public class MMSBJwtDemoApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MMSBJwtDemoApplication.class, args);
+	}
+
+}
---
+++ mm-sb-oauth2-client/src/main/java/com/mm/sb/oauth2/MmSbOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.mm.sb.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSbOauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSbOauth2ClientApplication.class, args);
+	}
+
+}
